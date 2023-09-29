beb20413416360307b522f2dd977ec4d75373f30
Nayem <nayemalam110@gmail.com>
2023-09-29 20:22:37 +0600

    design update

--- app/src/main/java/com/example/tinppy/MainActivity.kt
+++ app/src/main/java/com/example/tinppy/MainActivity.kt
@@ -1,25 +1,26 @@
 package com.example.tinppy
 
-import android.graphics.Color
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
-import java.util.Objects
-import kotlin.math.log
+import androidx.core.content.ContextCompat
+import androidx.vectordrawable.graphics.drawable.ArgbEvaluator
 
 private const val TAG = "MainActivity"
 private const val Intial_Tip = 15
+
 class MainActivity : AppCompatActivity() {
     private lateinit var base: EditText
     private lateinit var seekBarData: SeekBar
     private lateinit var tipp: TextView
     private lateinit var tipAmount: TextView
     private lateinit var total: TextView
+    private lateinit var comment: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,14 +31,15 @@ class MainActivity : AppCompatActivity() {
         tipAmount = findViewById(R.id.textView4)
         total = findViewById(R.id.textView5)
         seekBarData = findViewById(R.id.seekBarId)
-
+        comment = findViewById(R.id.commentId)
         seekBarData.progress = Intial_Tip
         tipp.text = "$Intial_Tip%"
-
+       updateTIp(Intial_Tip)
         seekBarData.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
 //Log.i(TAG, "progress Change $p1")
-                tipp.text = p1.toString() +"%"
+                tipp.text = p1.toString() + "%"
+                updateTIp(p1)
 
             }
 
@@ -52,7 +54,7 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-        base.addTextChangedListener(object : TextWatcher{
+        base.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
@@ -62,23 +64,41 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(p0: Editable?) {
-             computeTipTotal()
+                computeTipTotal()
+
             }
 
         })
 
     }
 
+    @SuppressLint("RestrictedApi")
+    private fun updateTIp(p1: Int) {
+       var tipDes = when(p1){
+
+           in 0..9 -> "poor"
+           in 10..14 ->"Good"
+           in 15..30 -> "Great"
+           else -> "amazing"
+       }
+        comment.text = tipDes
+        val ss = ArgbEvaluator().evaluate(
+            p1.toFloat()/seekBarData.max,ContextCompat.getColor(this,R.color.red),
+            ContextCompat.getColor(this,R.color.green)
+        ) as Int
+comment.setTextColor(ss)
+    }
+
     private fun computeTipTotal() {
-        if(base.text.isEmpty()){
-            tipAmount.text =""
+        if (base.text.isEmpty()) {
+            tipAmount.text = ""
             total.text = ""
             return
         }
         val baseAmount = base.text.toString().toDouble()
         val tipPercent = seekBarData.progress
-        val tipamount = baseAmount*tipPercent  / 100
-        val totalamount = baseAmount+tipamount
+        val tipamount = baseAmount * tipPercent / 100
+        val totalamount = baseAmount + tipamount
         tipAmount.text = "%.2f".format(tipamount)
         total.text = "%.2f".format(totalamount)
 
